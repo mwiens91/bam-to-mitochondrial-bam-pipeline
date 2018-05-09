cc0d86952c40b6268d8c5ba4e4179c378a07e76d
Matt Wiens <mwiens91@gmail.com>
2018-05-09 16:44:00 -0700

    Ensure not to do jobs already finished

--- bam_to_mt_bam.py
+++ bam_to_mt_bam.py
@@ -25,10 +25,15 @@ def main():
               " Exiting now.")
         sys.exit(1)
 
-    # This is used to interact with Azure blobs
-    block_blob_service = azure.storage.blob.BlockBlobService(
+    # Azure blob service for source
+    block_blob_service_source = azure.storage.blob.BlockBlobService(
          account_name=settings_dict['AZURE_SOURCE_STORAGE_ACCOUNT_NAME'],
          account_key=settings_dict['AZURE_SOURCE_STORAGE_ACCOUNT_KEY'],)
+
+    # Azure blob service for destination
+    block_blob_service_destination = azure.storage.blob.BlockBlobService(
+         account_name=settings_dict['AZURE_DESTINATION_STORAGE_ACCOUNT_NAME'],
+         account_key=settings_dict['AZURE_DESTINATION_STORAGE_ACCOUNT_KEY'],)
 
     # Get a list of blobs to download
     blob_list = []
@@ -36,14 +41,26 @@ def main():
     # Compile a list of all the BAM files for all of the chips we care
     # about.
     for cell_id in settings_dict['SOURCE_CHIP_IDS']:
+        # Get a list of the destination blobs for this cell so we don't
+        # duplicate work already done
+        destination_blobs_for_this_cell = (
+          block_blob_service_destination.list_blobs(
+            container_name=settings_dict['DESTINATION_STORAGE_CONTAINER_NAME'],
+            prefix=(settings_dict['SOURCE_CHIP_PATH_PREFIX']
+                    + '/'
+                    +  cell_id)))
+
         # Go through all files in the blob
-        for blob in block_blob_service.list_blobs(
+        for blob in block_blob_service_source.list_blobs(
                 container_name=settings_dict['SOURCE_STORAGE_CONTAINER_NAME'],
                 prefix=(settings_dict['SOURCE_CHIP_PATH_PREFIX']
                         + '/'
                         +  cell_id)):
-            # Filter out any non-BAM files
-            if blob.name.endswith('.bam'):
+            # Filter out any non-BAM files that we haven't already
+            # processed on a different run of this pipeline
+            if (blob.name.endswith('.bam')
+             and not bam_to_mt_bam_pipeline.rename_input_to_output(
+              blob.name) in destination_blobs_for_this_cell):
                 # Add the path of the blob
                 blob_list.append(blob.name)
 
